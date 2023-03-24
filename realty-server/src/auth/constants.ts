e20f96f1f6ce0ff7cd03d996f265bf05dc8e9d90
RomuloImajine <128384835+RomuloImajine@users.noreply.github.com>
2023-03-24 08:00:47 -0400

    feat(prisma): move prisma config to backend project

--- realty-server/src/auth/constants.ts
+++ realty-server/src/auth/constants.ts
@@ -4,5 +4,5 @@ export const IS_PUBLIC_KEY = 'isPublic';
 export const Public = () => SetMetadata(IS_PUBLIC_KEY, true);
 
 export const jwtConstants = {
-  secret: 'realty',
+  secret: process.env.JWT_KEY,
 };
