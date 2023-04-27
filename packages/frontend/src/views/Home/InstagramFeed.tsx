97d3f51a949daee9c72daecf1b4f31d7f8c5146e
RomuloImajine <128384835+RomuloImajine@users.noreply.github.com>
2023-04-27 09:15:27 -0400

    feat(feed): add instagram feed

---
+++ packages/frontend/src/views/Home/InstagramFeed.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './InstagramFeed.scss'
+
+const InstagramFeed = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-12">
+          <div className="section-title-area ltn__section-title-2--- text-center">
+            <h1 className="section-title">Instagram Feed</h1>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-lg-12 instagram-feed-container">
+          <iframe
+            src="https://www.juicer.io/api/feeds/romulocorona/iframe"
+            className="feed"
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default InstagramFeed
